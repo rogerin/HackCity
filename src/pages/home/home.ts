63ed16b951f94897ed6c98fd09cf93855d3d17b8
Rogerio Alencar Filho <geriofilho@gmail.com>
2018-07-07 17:50:01 -0300

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+
+import { Vibration } from '@ionic-native/vibration';
+import * as io from "socket.io-client";
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  socket: any;
 
+  constructor(platform: Platform, public navCtrl: NavController, private vibration: Vibration) {
+
+    platform.ready().then(() => {
+      this.socket = io.connect('URL');
+    })
+
+
+    this.socket.emit('message', { key: 'value' })
+    this.socket.on("connect", () => {})
+    this.socket.on("message", () => {})
+    
+
+  }
+
+
+  vibrar() {
+    this.vibration.vibrate(1000);
+
+    // Vibrate 2 seconds
+    // Pause for 1 second
+    // Vibrate for 2 seconds
+    // Patterns work on Android and Windows only
+    this.vibration.vibrate([2000,1000,2000]);
+
+    // Stop any current vibrations immediately
+    // Works on Android and Windows only
+    this.vibration.vibrate(0);
   }
 
 }
